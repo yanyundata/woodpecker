f96f376bea3bcf33ca76bf28c7fbd9f71222922d
Tom吴 <luvnana618@gmail.com>
2020-04-22 11:59:12 +0800

    init

--- demo/run.go
+++ demo/run.go
@@ -1,11 +1,15 @@
 package main
 
 import (
+	"fmt"
 	"github.com/yanyundata/woodpecker/apiUtils"
 	"log"
 )
 
 func main() {
-	json := apiUtils.GetA("http://httpbin.org/anything", apiUtils.Param{Data: map[string]string{"hello": "world"}}).ToJson()
+	json := apiUtils.GetA("https://yan-yun.com:38085/lvyuan/admin/test", apiUtils.Param{Data: map[string]string{"test": "123"}}).ToString()
+	if json == "123ok" {
+		fmt.Print("OK!!!")
+	}
 	log.Print(json)
 }
