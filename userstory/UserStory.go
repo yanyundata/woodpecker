055c7c5185ea879a740e9ddcee23f1b84dbaa614
Tom吴 <luvnana618@gmail.com>
2020-04-30 14:05:53 +0800

    add UserStory

--- userstory/UserStory.go
+++ userstory/UserStory.go
@@ -1,21 +1,26 @@
 package userstory
 
+import "container/list"
+
 type UserStory struct {
-	testCaseMap map[string]ITestCase
-	session     Session
+	testCaseMap   map[string]ITestCase
+	testCaseIndex *list.List
+	session       Session
 }
 
 func New() UserStory {
-	return UserStory{make(map[string]ITestCase), make(Session)}
+	return UserStory{make(map[string]ITestCase), list.New(), make(Session)}
 }
 
 func (us UserStory) Tell(name string, testCaseFun func(session Session) bool) UserStory {
 	us.testCaseMap[name] = TestCase(testCaseFun)
+	us.testCaseIndex.PushBack(name)
 	return us
 }
 
 func (us UserStory) ThatSAll() {
-	for name := range us.testCaseMap {
+	for e := us.testCaseIndex.Front(); e != nil; e = e.Next() {
+		name := e.Value.(string)
 		if us.testCaseMap[name].Test(us.session) {
 			println(name + " 测试通过")
 		} else {
