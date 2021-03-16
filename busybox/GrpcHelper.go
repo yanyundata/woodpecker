5fbe0d5803b4d4b25f2e21e9112996290531cd6b
Tom吴 <luvnana618@gmail.com>
2021-03-16 13:53:22 +0800

    add resty

--- busybox/GrpcHelper.go
+++ busybox/GrpcHelper.go
@@ -1,7 +1,9 @@
 package busybox
 
 import (
+	"crypto/tls"
 	grpc "google.golang.org/grpc"
+	"google.golang.org/grpc/credentials"
 	"log"
 )
 
@@ -10,6 +12,20 @@ type GrpcHelper struct {
 
 func (gh GrpcHelper) Conn(host string) *grpc.ClientConn {
 	conn, err := grpc.Dial(host, grpc.WithInsecure())
+	if err != nil {
+		log.Fatalf("Connect failed: %v", err)
+	}
+
+	return conn
+}
+
+func (gh GrpcHelper) ConnWithSSL(host string) *grpc.ClientConn {
+	config := &tls.Config{
+		InsecureSkipVerify: true,
+	}
+
+	conn, err := grpc.Dial(host, grpc.WithTransportCredentials(credentials.NewTLS(config)))
+
 	if err != nil {
 		log.Fatalf("Connect failed: %v", err)
 	}
